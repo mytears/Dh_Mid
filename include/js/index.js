a4cf91a9c89919725c4be4e309c2666014d0e1fe
mytears <mytears9@gmail.com>
2026-01-05 07:45:57 +0900

    Update index.js

--- include/js/index.js
+++ include/js/index.js
@@ -282,6 +282,7 @@ function setMainInterval() {
     if (m_status_time_chk > 60) {
         m_status_time_chk = 0;
         setCallWebToApp('STATUS', 'STATUS');
+        setDateTime();
     }
 
     m_dust_time_chk += 1;
